1ce983831d7789d2e9b6efa1e7d8fa7da3523ec3
Kurt404 <kurtbohol30@gmail.com>
2025-08-31 20:50:48 +0800

    Initial commit

---
+++ nge.py
@@ -0,0 +1,5 @@
+while True:
+    print("Enter number: ")
+    x=int(input())
+    if x==0 or x<0:
+        break;
---
+++ pool.py
@@ -0,0 +1,13 @@
+for x in range(1, 101):
+    print(f"{x}", end= " ")
+    if x%5==0:
+        print()
+
+
+
+
+    for x in range(1, 6):
+        for y in range(6, 11):
+            print(f"{x}{y}", end=" ")
+    print()
+    print()
