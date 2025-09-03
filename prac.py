c3c53bdaa526b7312a61e80edddb74a0c6bb7144
Kurt404 <kurtbohol30@gmail.com>
2025-09-03 22:42:49 +0800

    Removed multitable.py and prac.py from repo (kept local copies)

--- prac.py
+++
@@ -1,9 +0,0 @@
-num=int(input("Enter matrix: "))
-
-for x in range (1, num +1):
-    for y in range (1, num +1):
-        if x%2==0:
-            print(f"*", end=" ")
-        else:
-            print(f"*", end=" ")
-    print()
